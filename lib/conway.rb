cb2c86f59dbdf47a319511f06d64048169a39fe9
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-19 13:55:18 -0400

    more comments

--- lib/conway.rb
+++ lib/conway.rb
@@ -15,7 +15,21 @@ class Cell
   
 
   # this is where we decided to make module for logic, seemed like after last nights
-  # study breaking up things into small bits is best practice  
+  # study breaking up things into small bits is best practice
+
+  # yeah agreed
+  # our thinking was 1) find the neighbors (some cells have 8, some have <8)
+  # 2) check the neighbors & count up living
+  # 3) change the status
+  # tricky part is if you change the status of cell[2,3] right away, then the neighbor of that cell
+  # will find a new value for [2,3] when it checks 
+  # not sure how to handle?
+
+  # I think you can put 2 values into cell.  Status & new status, only having read the status when determining
+  # new status.  And on new iteration setting status to "new status" at beginning of turn.  Names sound bad now
+  
+
+
 end
 
 
@@ -35,9 +49,9 @@ class Board
 
   def fill_with_cells
     # loop through size, and create a row array for each #
-    loop through again and fill with that many cells
-    push the row array into the @cells array
-    nested arrays would have x & y & status or where would status eventually be 
+    # loop through again and fill with that many cells
+    # push the row array into the @cells array
+    # nested arrays would have x & y & status or where would status eventually be 
 
    # neighbor = x + 1, y || y+1 || y-1
    # what function would be used to dive into neighbors to count up living?
