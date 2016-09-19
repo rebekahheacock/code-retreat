81f571666bb83ffec9af3b508c124271444eb6c6
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-19 14:25:44 -0400

    add comments

--- lib/conway.rb
+++ lib/conway.rb
@@ -1,18 +1,28 @@
 # Sandi's rules
 
 class Board
-  def initialize(n, val)
+  def initialize(n, val, initial_state)
     @board = []
     fill_board
+    # initial_state.each(|coordinates| )
     end
 
     def fill_board
     n.times do |row_index|
       row = []
-      n.times {|column_index| row << val}
+      n.times {|column_index| row << Cell.new(row_index, column_index)}
       @board << row
     end
   end
+end
 
-  class Cell
-    def initialize(row, column)
+class Cell
+  def initialize(row, column)
+    @status = false
+    @coordinates = [row, column]
+  end
+
+  def 
+
+  end
+end
